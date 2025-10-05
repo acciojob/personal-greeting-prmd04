a2d699ef26e6a7787feda480fd5572270ad0cf35
pramod ade <prmdextracthere@gmail.com>
2025-10-05 17:20:20 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [input,setInput] = useState('');
   return (
     <div>
-        {/* Do not remove the main div */}
+     <label>Enter your name:</label><br/>
+       <input type="text" value={input} onChange={(e)=>setInput(e.target.value)}/>
+
+       <p>Hello, {input}</p>
     </div>
   )
 }
